f47448a2910d17621db505ed20003068eff691ed
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-02-20 11:01:52 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = (int)(((double) num1 / num2) * (double)1000);
+        return answer;
+    }
+}
