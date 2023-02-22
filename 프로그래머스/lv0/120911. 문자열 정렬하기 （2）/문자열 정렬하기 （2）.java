bfee5e4c08edd88c3f34e0ed7f7956752100a339
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-02-22 14:13:41 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.36 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -1,24 +1,39 @@
+//try-1-2023-02-22-java original
 import java.util.Arrays;
 
 class Solution {
     public String solution(String my_string) {
-        // convert to lowerCase
-        // use string.charAt() -> unicode int[]
-        // sort arrays
-        // int[] -> char concatenate
+        // char -> is integer
         String answer = "";
-        answer = my_string.toLowerCase();
-        
-        int[] my_stringToInt = new int[answer.length()];
-        for (int i = 0; i < answer.length(); i++) {
-            my_stringToInt[i] = (int) answer.charAt(i);
-        }
-        
-        Arrays.sort(my_stringToInt);
-        answer = "";
-        for (int i = 0; i < my_stringToInt.length; i++) {
-            answer += (char) my_stringToInt[i];
-        }
+        char[] charArray = my_string.toLowerCase().toCharArray();
+        Arrays.sort(charArray);
+        answer = String.valueOf(charArray);
         return answer;
     }
 }
+
+// //try-1-2023-02-22-java original
+// import java.util.Arrays;
+
+// class Solution {
+//     public String solution(String my_string) {
+//         // convert to lowerCase
+//         // use string.charAt() -> unicode int[]
+//         // sort arrays
+//         // int[] -> char concatenate
+//         String answer = "";
+//         answer = my_string.toLowerCase();
+        
+//         int[] my_stringToInt = new int[answer.length()];
+//         for (int i = 0; i < answer.length(); i++) {
+//             my_stringToInt[i] = (int) answer.charAt(i);
+//         }
+        
+//         Arrays.sort(my_stringToInt);
+//         answer = "";
+//         for (int i = 0; i < my_stringToInt.length; i++) {
+//             answer += (char) my_stringToInt[i];
+//         }
+//         return answer;
+//     }
+// }
