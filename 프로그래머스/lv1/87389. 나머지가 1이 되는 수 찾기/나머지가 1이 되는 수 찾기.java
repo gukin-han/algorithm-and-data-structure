210eca51eadbbdba1fc04152e8df929358033b8c
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-02-22 12:32:44 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.01 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -1,9 +1,23 @@
-// Try-1-2023-02-22-stream
-import java.util.stream.*;
-
+// Try-2-2023-02-22-java original
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        return IntStream.range(1, n).filter(num -> n % num == 1).min().getAsInt();
+        for (int i = 1; i < n; i++) {
+            if (n % i == 1) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
     }
 }
+
+// // Try-1-2023-02-22-stream
+// import java.util.stream.*;
+
+// class Solution {
+//     public int solution(int n) {
+//         int answer = 0;
+//         return IntStream.range(1, n).filter(num -> n % num == 1).min().getAsInt();
+//     }
+// }
