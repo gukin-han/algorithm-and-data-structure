85e33c8696ec15ec5c49a39201bde97a7445cae3
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-02-27 08:11:18 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 1.68 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        my_string += "A";
+        int i = 0;
+        int j = 0;
+        
+        while (i < my_string.length()) {
+            if (isDigit(my_string.charAt(i))) {
+                j = i;
+                while (j < my_string.length()) {
+                    if (isDigit(my_string.charAt(j))) {
+                        j++;
+                    } else {
+                        answer += Integer.parseInt(my_string.substring(i, j));
+                        i = j;
+                        break;
+                    }
+                }
+            } else {
+                i++;
+            }
+        }
+        return answer;
+    }
+    
+    boolean isDigit(char c) {
+        if ( ((int) c) >= 48 && ((int) c) <= 57) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
