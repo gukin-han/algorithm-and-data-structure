f10bb16b6d6af9f5b6b159fedc033fd078f6276f
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-03-15 11:09:39 +0900

    [Bronze III] Title: 최소, 최대, Time: 1772 ms, Memory: 318244 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.print(getMin(array));
+        System.out.print(" "+ getMax(array));
+    }
+
+    static int getMax(int[] array) {
+        int tempMax = Integer.MIN_VALUE;
+        for (int x : array) {
+            if (x > tempMax) {
+                tempMax = x;
+            }
+        }
+        return tempMax;
+    }
+
+    static int getMin(int[] array) {
+        int tempMin = Integer.MAX_VALUE;
+        for (int x : array) {
+            if (x < tempMin) {
+                tempMin = x;
+            }
+        }
+        return tempMin;
+    }
+}
