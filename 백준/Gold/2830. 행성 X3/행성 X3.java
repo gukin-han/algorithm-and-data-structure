80080d29c4b7b3189cb48edf8cebb3da1a56479b
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-03-20 17:01:07 +0900

    [Gold III] Title: 행성 X3, Time: 436 ms, Memory: 64516 KB -BaekjoonHub

---
+++ 백준/Gold/2830. 행성 X3/행성 X3.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static final int DIGIT = 20;
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int residentsNum = Integer.parseInt(reader.readLine());
+        int[] oneCount = new int[DIGIT];
+        for (int i = 0; i < residentsNum; i++) {
+            int residentName = Integer.parseInt(reader.readLine());
+            for (int j = 0; j < 20; j++) {
+                if ((residentName & (1 << j)) > 0) oneCount[j]++;
+            }
+        }
+
+        long answer = 0L;
+        for (int i = 0; i < DIGIT; i++) {
+            int zeroCount = residentsNum - oneCount[i];
+            answer += (1L << i) * zeroCount * oneCount[i];
+        }
+        System.out.println(answer);
+    }
+}
