a5e99e4a121a4013ff58918d9074e11b85b02041
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-03-21 18:57:29 +0900

    [level 2] Title: 위장, Time: 0.34 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.java
+++ 프로그래머스/lv2/42578. 위장/위장.java
@@ -1,28 +1,24 @@
-import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 
 class Solution {
     public int solution(String[][] clothes) {
-        
-        Map<String,Integer> map = new HashMap();
-        int initialValue = 2;
-        
-        for (String[] cloth: clothes) {
-            if (!map.containsKey(cloth[1])) {
-                map.put(cloth[1], initialValue);
-            } else {
-                map.put(cloth[1], (map.get(cloth[1]) + 1));
-            }  
-        }
-        
-        
         int answer = 1;
-
-        for (Map.Entry<String, Integer> item : map.entrySet()) {
-            answer *= item.getValue();
+        
+        Hashtable<String, Integer> ht = new Hashtable<>();
+        for (String[] clothSet: clothes) {
+            String currentCloth = clothSet[1];
+            if (ht.containsKey(currentCloth)) {
+                ht.put(currentCloth, (ht.get(currentCloth) + 1));
+            } else {
+                ht.put(currentCloth, 1);
+            }
         }
-
-
+        
+        for (Map.Entry<String, Integer> item: ht.entrySet()) {
+            answer *= (item.getValue() + 1);
+        }
+        
         return answer - 1;
     }
 }
