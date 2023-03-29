38bd253622f9469793119afb2ae42fcc3b1a6df1
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-03-29 16:20:43 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 9.39 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        s = s.toLowerCase();
+        
+        int wordIndex = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == ' ') {
+                wordIndex = 0;
+                answer += ch;
+                continue;
+            } else if (isEven(wordIndex) && (ch >= 'a' && ch <= 'z')) {
+                ch = (char) (ch + 'A'-'a');
+            }
+        
+            answer += ch;
+            wordIndex++;
+        }
+        return answer;
+    }
+    
+    boolean isEven(int i) {
+        return (i % 2 == 0);
+    }
+}
