0ba7ad6602b70c5a157696f11d3d88cff284b5aa
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-03-29 16:33:27 +0900

    [level 1] Title: 예산, Time: 0.35 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.java
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -1,17 +1,12 @@
 import java.util.Arrays;
-
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
         Arrays.sort(d);
-        int sum = 0;
         for (int i = 0; i < d.length; i++) {
-            sum += d[i];
-            if (budget >= sum) {
-                answer++;
-            } else {
-                break;
-            }
+            budget -= d[i];
+            if (budget < 0) break;
+            answer++;
         }
         return answer;
     }
