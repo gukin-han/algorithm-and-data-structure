d2a2d94edc95e85e1dee3618a402ab95f803f911
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-04-04 11:51:53 +0900

    [level 1] Title: 소수 찾기, Time: 20.54 ms, Memory: 53.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n) {
+        
+        boolean[] isPrime = new boolean[n+1];
+        for (int i = 2; i < isPrime.length; i++) {
+            isPrime[i] = true;
+        }
+        
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * 2; j <= n; j += i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+        int answer = 0;
+        for (int i = 0; i < isPrime.length; i++) {
+            if(isPrime[i]) {
+                answer ++;
+            }
+        }
+        
+        return answer;
+    }
+}
