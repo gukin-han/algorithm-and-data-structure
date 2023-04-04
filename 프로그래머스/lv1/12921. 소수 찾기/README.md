f13c3aa016c79dab30358039eaf4896848abb3c3
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-04-04 12:27:10 +0900

    [level 1] Title: 소수 찾기, Time: 23.05 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/README.md
+++ 프로그래머스/lv1/12921. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 소수 찾기 - 12921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12921?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12921) 
 
 ### 성능 요약
 
-메모리: 53.3 MB, 시간: 20.54 ms
+메모리: 52.9 MB, 시간: 23.05 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -8,7 +8,7 @@ class Solution {
         
         for (int i = 2; i * i <= n; i++) {
             if (isPrime[i]) {
-                for (int j = i * 2; j <= n; j += i){
+                for (int j = i * i; j <= n; j += i){
                     isPrime[j] = false;
                 }
             }
