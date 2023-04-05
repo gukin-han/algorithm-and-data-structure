be0a816504df8363ed11644220873bfb53a5b516
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-04-05 23:49:00 +0900

    [level 2] Title: 구명보트, Time: 10.14 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -2,12 +2,32 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] people, int limit) {
+        int answer = 0;
+
         Arrays.sort(people);
-        int i = 0, j = people.length - 1;
-        for (; i < j; --j) {
-            if (people[i] + people[j] <= limit)
-                ++i;
+        boolean[] visited = new boolean[people.length];
+
+        int index = people.length - 1;
+        for (int i = 0; i < people.length; ++i) {
+            if (!visited[i]) {
+                int person = people[i];
+                visited[i] = true;
+                ++answer;
+
+                for (int j = index; j > i; --j) {
+                    if (!visited[j]) {
+                        int partner = people[j];
+                        if (partner + person <= limit) {
+                            visited[j] = true;
+                            index = j - 1;
+                            break;
+                        }
+                    }
+                    --index;
+                }
+            }            
         }
-        return people.length - i;
+
+        return answer;
     }
 }
