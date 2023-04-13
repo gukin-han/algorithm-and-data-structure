208cb6e010624300cf18b18f3ee49a0b1ac9fbd3
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-04-13 22:44:20 +0900

    [level 1] Title: 약수의 합, Time: 0.10 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -1,19 +1,11 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        
-        if (n == 0) {
-            return 0;
-        }
-        
-        for (int i = 1; i <= n/2 ; i++) {
+        for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
                 answer += i;
             }
         }
-        
-        answer += n;
-        
         return answer;
     }
 }
