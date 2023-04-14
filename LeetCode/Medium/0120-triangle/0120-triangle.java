ef9ac6dc46899976221f2c128e4882dded2acacc
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-04-14 15:45:06 +0900

    Time: 1 ms (99.85%), Space: 42.2 MB (65.31%) - LeetHub

--- LeetCode/Medium/0120-triangle/0120-triangle.java
+++ LeetCode/Medium/0120-triangle/0120-triangle.java
@@ -1,19 +1,21 @@
 class Solution {
-    private Integer[][] memo;
-
-public int minimumTotal(List<List<Integer>> triangle) {
-	int n = triangle.size();
-	memo = new Integer[n][n];
-	return dfs(0, 0, triangle);
-}
-
-private int dfs(int level, int i, List<List<Integer>> triangle) {
-	if (memo[level][i] != null) return memo[level][i];
-
-	int path = triangle.get(level).get(i);
-	if (level < triangle.size() - 1) 
-		path += Math.min(dfs(level + 1, i, triangle), dfs(level + 1, i + 1, triangle));
-
-	return memo[level][i] = path;
-}
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle == null || triangle.size() == 0) return 0;
+        Integer[][] cache = new Integer[triangle.size()][triangle.size()];
+        return dfs(0, 0,triangle, cache);
+    }
+    
+    
+    int dfs(int row, int pos, List<List<Integer>> triangle, Integer[][] cache){
+        //Out of bounds so just return sum previous value must be leaf node
+        if (row+1 >= triangle.size()) {
+            return triangle.get(row).get(pos); 
+        }
+        if(cache[row][pos] != null) return cache[row][pos];
+        
+      cache[row][pos] =  triangle.get(row).get(pos) + Math.min ( dfs(row+1, pos,triangle, cache), dfs(row+1, pos+1,triangle, cache));
+        
+        return cache[row][pos];
+        
+    }
 }
