0d828e7bbf8c6795e8e1aa8b221000d1ee32565f
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-04-14 14:17:46 +0900

    Time: 3 ms (68.73%), Space: 42.4 MB (43.19%) - LeetHub

--- LeetCode/Medium/0120-triangle/0120-triangle.java
+++ LeetCode/Medium/0120-triangle/0120-triangle.java
@@ -1,34 +1,12 @@
-import java.util.*;
-
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        int[][] memo = new int[n][n];
-        for (int i = 0; i < memo.length; i++) {
-            for (int j = 0; j < memo[i].length; j++) {
-                memo[i][j] = 10_000_000;
-            }
-        }
-        memo[0][0] = triangle.get(0).get(0);
-        
-        for (int i = 0; i < triangle.size(); i++) {
-            for (int j = 0; j < triangle.get(i).size(); j++) {
-                int step = memo[i][j];
-                
-                
-                if (i + 1 < triangle.size()) {
-                    memo[i+1][j] = Math.min(memo[i+1][j], step + triangle.get(i+1).get(j));
-                    memo[i+1][j+1] = Math.min(memo[i+1][j+1], step + triangle.get(i+1).get(j+1));
-                    System.out.println(memo[i+1][j] + " " + memo[i+1][j+1]);
-                }
-             }
-        }
-        int answer = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            answer = Math.min(answer, memo[n-1][i]);
-            
-        }
-        
-        return answer;
-    }
+	int n = triangle.size();
+	int[][] dp = new int[n+1][n+1];
+
+	for (int level=n-1; level>=0; level--)
+		for (int i=0; i<=level; i++)
+			dp[level][i] = triangle.get(level).get(i) + Math.min(dp[level+1][i], dp[level+1][i+1]);
+
+	return dp[0][0];
+}
 }
