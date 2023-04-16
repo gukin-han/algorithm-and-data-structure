e3ff04930d7b96f417103a8c79c900585599f4b8
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-04-16 10:37:30 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.java
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -1,16 +1,6 @@
 class Solution {
     public int solution(int angle) {
-        
-        if (0 < angle && angle < 90) {
-            return 1;
-        } else if (angle == 90) {
-            return 2;
-        } else if (90 < angle && angle < 180) {
-            return 3;
-        } else if (angle == 180) {
-            return 4;
-        } else {
-            return -1;
-        }
+        int answer = 0;
+        return (angle < 90)? 1: (angle == 90)? 2: (angle < 180)? 3: (angle == 180)? 4: -1;
     }
 }
