4fa32a6dc72d2c5fd3f373a1b59f2ad92c7d8b9b
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-04-17 10:38:49 +0900

    [level 0] Title: 짝수의 합, Time: 1.37 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -1,9 +1,7 @@
+import java.util.stream.*;
+
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        for (int i = 2; i <= n ; i = i + 2) {
-            answer+= i;
-        }
-        return answer;
+        return IntStream.range(2, n+1).filter(e -> e % 2 == 0).sum();
     }
 }
