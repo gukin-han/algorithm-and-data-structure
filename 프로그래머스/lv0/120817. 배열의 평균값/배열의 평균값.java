ecdbb5704fa8b684d28c62d6ef39250ab383cd7a
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-04-17 10:27:00 +0900

    [level 0] Title: 배열의 평균값, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,43 +1,10 @@
-// Try-3-2023-02-22-ternary operator, stream class
-import java.util.Arrays;
-
 class Solution {
     public double solution(int[] numbers) {
         double answer = 0;
-        return Arrays.stream(numbers).average().orElse(0);
+        for (int number: numbers) {
+            answer += number;
+        }
+        answer /= numbers.length;
+        return answer;
     }
 }
-
-// // Try-2-2023-02-22
-// class Solution {
-//     public double solution(int[] numbers) {
-//         double answer = 0;
-//         int numbersSum = 0;
-//         double numberCnt = 0;
-        
-//         for (int num: numbers) {
-//             numbersSum += num;
-//             numberCnt++;
-//         }
-        
-//         answer = numbersSum / numberCnt;
-//         return answer;
-//     }
-// }
-
-// // Try-1
-// class Solution {
-//     public double solution(int[] numbers) {
-//         int sum = 0;
-        
-//         for(int number: numbers) {
-//             sum += number;
-//         }
-        
-//         double arrayLength = numbers.length;
-        
-        
-//         double answer = (sum/arrayLength);
-//         return answer;
-//     }
-// }
