6ee2af1f3b7b88ee0882ea95ba6bad7cc31c52cb
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-04-25 10:50:48 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -1,12 +1,9 @@
-//review?-no-2023-02-26-sun-naitve
 class Solution {
     public int solution(int[] array, int n) {
         int answer = 0;
-        int[] possibleElement = new int[1000];
-        for (int item: array) {
-            possibleElement[item]++;
+        for (int element: array) {
+            if (element == n) answer++;
         }
-        answer = possibleElement[n];
         return answer;
     }
 }
