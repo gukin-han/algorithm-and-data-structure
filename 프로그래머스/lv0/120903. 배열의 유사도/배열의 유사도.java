a8bd8ac5a6f561852b9e1fdcc832e1ed33601189
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-04-28 11:29:47 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
@@ -1,15 +1,11 @@
 class Solution {
     public int solution(String[] s1, String[] s2) {
-        // Use nested for loop and equals method and count up
         int answer = 0;
         for (String str1: s1) {
             for (String str2: s2) {
-                if (str1.equals(str2)) {
-                    answer++;
-                }
+                if (str1.equals(str2)) answer++;
             }
         }
-        
         return answer;
     }
 }
