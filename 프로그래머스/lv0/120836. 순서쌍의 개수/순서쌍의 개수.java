9315d224cc7ac79bdadd6b3f93e7559c313ee67d
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-04-28 10:57:24 +0900

    [level 0] Title: 순서쌍의 개수, Time: 6.14 ms, Memory: 65.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,14 +1,11 @@
 class Solution {
     public int solution(int n) {
-        // Create int array with the size of n
-        // Find the divisor of n and assign it to the int array
-        // Return the number of the divisor
-        int numOfTheDivisor = 0;
-        for (int divisor = 1; divisor <= n; divisor++) {
-            if (n % divisor == 0) {
-                numOfTheDivisor++;
-            }
+        int answer = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) answer++;
         }
-        return numOfTheDivisor;
+        
+        return answer;
     }
 }
