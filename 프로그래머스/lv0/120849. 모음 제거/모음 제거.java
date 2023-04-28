274da4c25e4659d2ce343d314b4d84d7309015ea
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-04-28 10:54:23 +0900

    [level 0] Title: 모음 제거, Time: 0.13 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,27 +1,5 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        // if there is 'a', 'e', 'i', 'o', 'u' - vowels check
-        // not include this char in newly create object with StringBuffer class
-        // use StringBuffer class for frequent adding and deleting instruction
-        for (int i = 0; i < my_string.length(); i++) {
-            if (isVowel(my_string.charAt(i))) {
-                continue;
-            } else {
-                answer += my_string.charAt(i);
-            }
-        }
-        
-        return answer;
-    }
-    
-    public boolean isVowel(char c) { 
-        // check - https://www.wordexample.com/list/most-common-vowels
-        // e -> a -> i -> o -> u is the most frequently used vowels
-        if (c == 'e' || c == 'a' || c == 'i' || c == 'o' || c == 'u') {
-            return true;
-        } else {
-            return false;
-        }
+        return my_string.replaceAll("[aeiou]","");
     }
 }
