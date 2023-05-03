4faa5c03dcc2d8119c9eaf567312c591d50bf6c0
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-05-03 23:58:50 +0900

    Time: 11 ms (18.72%), Space: 46.7 MB (58.50%) - LeetHub

--- LeetCode/Easy/0338-counting-bits/0338-counting-bits.java
+++ LeetCode/Easy/0338-counting-bits/0338-counting-bits.java
@@ -1,13 +1,14 @@
 class Solution {
-    public int[] countBits(int n) {        
-        int[] dp = new int[n+1];
-        if (n == 0) return dp;
+    public int[] countBits(int n) {
+        int[] answer = new int[n+1];
         
-        int offset = 1;
-        for (int i = 1; i < dp.length; i++) {
-            if (i == offset * 2) offset *=2;
-            dp[i] = dp[i - offset] + 1;
+        for (int i = 0; i < answer.length; i++) {
+            int index = i;
+            while (index != 0) {
+                answer[i] += index % 2;
+                index = index / 2;
+            }
         }
-        return dp;
+        return answer;
     }
 }
