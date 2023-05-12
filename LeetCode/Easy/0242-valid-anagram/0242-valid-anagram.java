61ceafc10aec3eb9be3256b7696685de7017305b
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-05-12 13:52:19 +0900

    Time: 14 ms (31.46%), Space: 43.8 MB (6.00%) - LeetHub

---
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        
+        Map<Character, Integer> map = new HashMap<>();
+        for (Character ch: s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for (Character ch: t.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) - 1);
+            if (map.get(ch) < 0) return false;
+        }
+        
+        
+        return true;
+    }
+}
