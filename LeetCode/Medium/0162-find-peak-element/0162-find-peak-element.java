8f49a4f08ad834a16c3d2b4a0ccfce3fcb73bdfb
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-05-12 13:42:43 +0900

    Time: 0 ms (100.00%), Space: 42 MB (80.85%) - LeetHub

--- LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.java
+++ LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.java
@@ -1,16 +1,27 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
         
-        while (left < right) {
+        if (nums.length == 1) return 0;
+        
+        // check edge cases
+        if (nums[0] > nums[1]) return 0;
+        if (nums[nums.length - 2] < nums[nums.length - 1]) return nums.length - 1;
+        
+        int left = 1;
+        int right = nums.length - 2;
+        
+        while (left <= right) {
             int mid = (left + right)/2;
-            if (nums[mid + 1] > nums[mid]) {
+            if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) {
+                return mid;
+            } else if (nums[mid-1] > nums[mid]) {
+                right = mid - 1;
+            } else if (nums[mid] < nums[mid+1]) {
                 left = mid + 1;
-            } else {
-                right = mid;
             }
         }
-        return right;
+        
+        return -1;
+    
     }
 }
