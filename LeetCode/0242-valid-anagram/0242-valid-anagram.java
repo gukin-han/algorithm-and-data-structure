daffa411125171d2913ea0507190f53788770589
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-05-16 14:12:18 +0900

    Time: 2 ms (99.71%), Space: 42.8 MB (64.88%) - LeetHub

--- LeetCode/0242-valid-anagram/0242-valid-anagram.java
+++ LeetCode/0242-valid-anagram/0242-valid-anagram.java
@@ -3,11 +3,11 @@ class Solution {
         if (s.length() != t.length()) return false;
         int[] charCount = new int[(int) ('z' - 'a' + 1)];
 
-        for (Character ch: s.toCharArray()) {
+        for (char ch: s.toCharArray()) {
             charCount[(int) (ch - 'a')]++;
         }
         
-        for (Character ch: t.toCharArray()) {
+        for (char ch: t.toCharArray()) {
             charCount[(int) (ch - 'a')]--;
             if (charCount[(int) (ch - 'a')] < 0) return false;
         }
