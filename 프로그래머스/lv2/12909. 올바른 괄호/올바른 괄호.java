8bfc928fe842a5c867f33e5d368806e012c45545
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-05-17 17:33:16 +0900

    [level 2] Title: 올바른 괄호, Time: 6.90 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,18 +2,12 @@ class Solution {
     boolean solution(String s) {
         int balance = 0;
         for (char ch: s.toCharArray()) {
-            switch(ch) {
-                case '(': balance++; break;
-                case ')': balance--; break;
-            }
-            if (balance < 0) {
-                return false;
-            }
+            if (ch == '(') balance++;
+            else balance--;
+            
+            if (balance < 0) return false;
         }
-        if (balance == 0) {
-            return true;
-        }
-
-        return false;
+        if (balance != 0) return false;
+        return true;
     }
 }
