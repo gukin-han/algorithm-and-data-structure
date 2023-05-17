6cd5f449efc646f75e2c4336eaadf6a28973cc33
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-05-17 17:38:47 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 7.90 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,20 +1,15 @@
+import java.util.*;
+
 class Solution {
     public String solution(String s) {
-        String[] strArr = s.split(" ");
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        
-        for (int i = 0; i < strArr.length; i++) {
-            int cur = Integer.parseInt(strArr[i]);
-            max = Math.max(cur, max);
-            min = Math.min(cur, min);
+        String[] strArray = s.split(" ");
+        int[] intArray = new int[strArray.length];
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = Integer.parseInt(strArray[i]);
         }
         
-        StringBuilder sb = new StringBuilder();
-        sb.append(String.valueOf(min))
-            .append(" ")
-            .append(String.valueOf(max));
+        Arrays.sort(intArray);
         
-        return sb.toString();
+        return "" + intArray[0] + " " + intArray[intArray.length - 1];
     }
 }
