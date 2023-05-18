a5fe436ce377627765b85a0970792b1ad580ae58
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-05-18 12:46:29 +0900

    [unrated] Title: 공배수, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181936. 공배수/공배수.java
+++ 프로그래머스/unrated/181936. 공배수/공배수.java
@@ -1,5 +1,6 @@
 class Solution {
     public int solution(int number, int n, int m) {
-        return (number % n == 0 && number % m == 0)? 1:0;
+        if (number % n == 0 && number % m == 0) return 1;
+        return 0;
     }
 }
