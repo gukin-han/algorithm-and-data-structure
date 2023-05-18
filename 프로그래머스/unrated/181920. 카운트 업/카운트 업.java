24c41b70389e0b681af291026a05aebb896c6669
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-05-18 12:50:54 +0900

    [unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] solution(int start, int end) {
         int[] answer = new int[end - start + 1];
         for (int i = 0; i < answer.length; i++) {
-            answer[i] = i + start;
+            answer[i] = start + i;
         }
         return answer;
     }
