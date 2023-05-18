62f6ef08e509e0413248f738d97934d072018f51
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-05-18 12:49:33 +0900

    [unrated] Title: 길이에 따른 연산, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -2,16 +2,15 @@ class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
         if (num_list.length >= 11) {
-            for (int num: num_list) {
-                answer += num;
+            for (int x: num_list) {
+                answer+= x;
             }
         } else {
             answer++;
-            for (int num: num_list) {
-                answer *= num;
+            for (int x: num_list) {
+                answer*= x;
             }
         }
-        
         return answer;
     }
 }
