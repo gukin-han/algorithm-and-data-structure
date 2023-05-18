f57d1a5384e39f16a5fbe8f4ff756c80a0fa8743
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-05-18 13:25:33 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.01 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] solution(int[] num_list, int n) {
         int[] answer = new int[n];
-        
         for (int i = 0; i < n; i++) {
             answer[i] = num_list[i];
         }
