80cae4c6562a334d05ce29ac79c7cbcf268f8b31
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-05-18 13:34:57 +0900

    [unrated] Title: 공백으로 구분하기 1, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -1,5 +1,6 @@
 class Solution {
     public String[] solution(String my_string) {
-        return my_string.split(" ");
+        String[] answer = my_string.split(" ");
+        return answer;
     }
 }
