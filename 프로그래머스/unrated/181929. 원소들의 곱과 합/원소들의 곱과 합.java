281c48a6163adffc6e0038a57a6819ac92cc0805
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-05-18 13:38:19 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,11 +1,14 @@
 class Solution {
     public int solution(int[] num_list) {
+        int multiplication = 1;
         int sum = 0;
-        int mult = 1;
-        for (int i = 0; i < num_list.length; i++ ) {
-            sum += num_list[i];
-            mult *= num_list[i];
+        
+        for (int i: num_list) {
+            multiplication *= i;
+            sum += i;
         }
-        return (sum*sum > mult)? 1:0;
+        
+        sum *= sum;
+        return (multiplication < sum)? 1: 0;
     }
 }
