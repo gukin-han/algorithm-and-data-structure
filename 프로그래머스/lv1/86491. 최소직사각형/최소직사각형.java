ddf1091af41fb6fc512b92e26f0bf9ce76475bcd
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-05-19 20:48:18 +0900

    [level 1] Title: 최소직사각형, Time: 1.93 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -1,8 +1,7 @@
 class Solution {
     public int solution(int[][] sizes) {
-        int w = 0;
-        int h = 0;
-        
+        int w = 0; // 가로 길이
+        int h = 0; // 세로 길이
         for (int[] size: sizes) {
             w = Math.max(w, Math.max(size[0], size[1]));
             h = Math.max(h, Math.min(size[0], size[1]));
