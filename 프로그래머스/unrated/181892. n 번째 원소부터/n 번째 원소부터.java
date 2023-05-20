860ee812817b1d8a0674bfa5adee1a86f5e758be
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-05-20 11:14:16 +0900

    [unrated] Title: n 번째 원소부터, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[num_list.length + 1 - n];
+        int[] answer = new int[num_list.length - n + 1];
         for (int i = 0; i < answer.length; i++) {
-            answer[i] = num_list[i+n-1];
+            answer[i] = num_list[n - 1 + i];
         }
         return answer;
     }
