f2adca41180ea7407c2384b8d956a18f719d3cdb
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-05-20 11:24:32 +0900

    [unrated] Title: 수 조작하기 1, Time: 0.98 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,11 +1,12 @@
 class Solution {
     public int solution(int n, String control) {
-        for (char ch: control.toCharArray()) {
+
+        for(char ch:control.toCharArray()) {
             switch(ch) {
                 case 'w': n++; break;
                 case 's': n--; break;
-                case 'd': n += 10; break;
-                case 'a': n -= 10; break;
+                case 'd': n+=10; break;
+                case 'a': n-=10;
             }
         }
         return n;
