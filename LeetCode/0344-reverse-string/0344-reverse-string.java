a449018ec757c13b432b7b6d18d7ac5330780db6
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-05-21 10:18:07 +0900

    Time: 1 ms (95.03%), Space: 50.9 MB (5.12%) - LeetHub

--- LeetCode/0344-reverse-string/0344-reverse-string.java
+++ LeetCode/0344-reverse-string/0344-reverse-string.java
@@ -1,10 +1,10 @@
 class Solution {
     public void reverseString(char[] s) {
-        char temp;
+        byte temp;
         for(int i = 0; i < s.length/2; i++) {
-            temp = s[s.length - 1 - i];
+            temp = (byte) s[s.length - 1 - i];
             s[s.length - 1 - i] = s[i];
-            s[i] = temp;
+            s[i] = (char) temp;
         }
     }
 }
