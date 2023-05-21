e77b96c4db628b777c6873c3068c8fcb66e57dd3
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-05-21 12:27:49 +0900

    [level 0] Title: 중복된 문자 제거, Time: 8.80 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -1,20 +1,10 @@
-import java.util.LinkedHashSet;
-
 class Solution {
     public String solution(String my_string) {
-        // Use LinkedHashSet to ignore duplicates while adding and to printout in the order.
-        // Create instance of LinkedHashSet class where characters from my_string would be stored.
-        // Put char of splited my_string in this object.
-        // Concatenate chars to answer.
-        
-        LinkedHashSet<Character> linkedCharacterSet = new LinkedHashSet<Character>();
-        for (int i=0; i < my_string.length(); i++) {
-            linkedCharacterSet.add(my_string.charAt(i));
-        }
-        
         String answer = "";
-        for (char ch: linkedCharacterSet) {
-            answer += ch;
+        for (int i = 0; i < my_string.length(); i++) {
+            if (my_string.indexOf(my_string.charAt(i)) == i) {
+                answer += my_string.charAt(i);
+            }
         }
         return answer;
     }
