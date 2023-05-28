983db571333585a0bbd209dcde73180f8d9dab72
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-05-28 14:10:37 +0900

    Time: 3 ms (52.75%), Space: 41.9 MB (99.74%) - LeetHub

--- LeetCode/0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ LeetCode/0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -4,23 +4,27 @@ class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
+        
+        int p1 = 0, p2 = 0;
         int n = nums1.length, m = nums2.length;
-        int i = 0, j = 0;
         List<Integer> list = new ArrayList<>();
-        while(i < n && j < m){
-            int a = nums1[i], b= nums2[j];
-            if(a == b){
-                list.add(a);
-                i++;
-                j++;
-            }else if(a < b){
-                i++;
-            }else{
-                j++;
+        while (n > p1 && m > p2) {
+            int num1 = nums1[p1], num2 = nums2[p2];
+            if (num1 == num2) {
+                list.add(num1);
+                p1++;
+                p2++;
+            } else if (num1 > num2) {
+                p2++;
+            } else if (num1 < num2) {
+                p1++;
             }
         }
+        
         int[] ret = new int[list.size()];
-        for(int k = 0; k < list.size();k++) ret[k] = list.get(k);
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = list.get(i);
+        }
         return ret;
     }
 }
