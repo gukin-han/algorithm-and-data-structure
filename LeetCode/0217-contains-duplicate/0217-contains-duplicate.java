67b1e7986233daf84776082fea31b51cf68c77f0
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-06-16 16:52:15 +0900

    Time: 13 ms (52.75%), Space: 54.6 MB (97.03%) - LeetHub

--- LeetCode/0217-contains-duplicate/0217-contains-duplicate.java
+++ LeetCode/0217-contains-duplicate/0217-contains-duplicate.java
@@ -2,9 +2,9 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int num: nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-            if(map.get(num) >= 2) return true;
-        }
+            if(map.containsKey(num)) return true;
+            else map.put(num, 1);
+        } 
         
         return false;
     }
