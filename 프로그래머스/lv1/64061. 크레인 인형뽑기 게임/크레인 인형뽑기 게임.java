19292749ffc579a537705c11d71eb67019dc9b4e
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-07-18 11:14:52 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.73 ms, Memory: 65.2 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -2,32 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        int answer = 0;
         Stack<Integer> bucket = new Stack<>();
-
-        for (int move: moves) {
-            int pick = pickUp(board, move-1);
-            if (pick == 0) {
-                continue;
-            } else if (bucket.size() == 0) {
-                bucket.push(pick);
-            } else if (bucket.peek() == pick) {
-                bucket.pop();
-                answer += 2;
-            } else {
-                bucket.push(pick);
+        int answer = 0;
+        
+        for(int move:moves) {
+            int pickedUp = pickUp(board, move);
+            if (pickedUp != 0) { // picked up somthing
+                if (bucket.isEmpty() || bucket.peek() != pickedUp) bucket.push(pickedUp);
+                else if(bucket.peek() == pickedUp) {
+                    bucket.pop();
+                    answer += 2;
+                }
             }
         }
         return answer;
     }
     
-    public int pickUp(int[][] board, int move) {
-        for (int i = 0; i < board.length; i ++) {
-            int pick = board[i][move];
-            if (pick != 0) {
-                board[i][move] = 0;
-                return pick;
-            }
+    private int pickUp(int[][] board, int move) {
+        int col = move - 1;
+        for (int row = 0; row < board.length; row++) {
+            int pickedUp = board[row][col];
+            if (pickedUp != 0) {
+                board[row][col] = 0; // remove
+                return pickedUp;
+            };
         }
         return 0;
     }
