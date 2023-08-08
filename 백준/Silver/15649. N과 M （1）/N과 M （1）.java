1303f3e94a57588a32185d419a2554c7afd0625c
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-08-08 20:39:53 +0900

    [Silver III] Title: N과 M (1), Time: 376 ms, Memory: 42784 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,44 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static List<Integer> temp = new ArrayList<>();
+    private static StringBuilder answer = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(reader.readLine());
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n];
         boolean[] visited = new boolean[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = i + 1;
-        }
-        int[] output = new int[m];
-
-        permutation(arr, visited, 0, m, output);
-
+        getCombination(n, r, visited);
+        System.out.println(answer.toString());
     }
 
-    public static void permutation(int[] arr, boolean[] visited, int depth, int m, int[] output) {
-        if (depth == m) {
-            for (int i = 0; i < output.length; i++) {
-                System.out.print(output[i] + " ");
+    private static void getCombination(int n, int r, boolean[] visited) {
+        if (r == 0) {
+            for (Integer i : temp) {
+                answer.append(i).append(" ");
             }
-            System.out.println();
-            return;
+            answer.append("\n");
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            if(!visited[i]){
-                visited[i] = true;
-                output[depth] = arr[i];
-                permutation(arr, visited, depth + 1, m, output);
-                visited[i] = false;
-            }
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            temp.add(i + 1);
+            getCombination(n, r - 1, visited);
+            visited[i] = false;
+            temp.remove(temp.size() - 1);
         }
     }
 }
