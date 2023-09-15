a8986fb9ef9704247c9c468e42f7d1c07c2fe855
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-09-15 22:14:54 +0900

    Time: 2205 ms (5.04%), Space: 146.9 MB (5.01%) - LeetHub

--- LeetCode/1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ LeetCode/1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,30 +1,65 @@
 class Solution {
+    
+    
+    private class Vertex implements Comparable<Vertex>{
+        Integer from;
+        Integer to;
+        Integer distance;
+        
+        private Vertex (int from, int to, int distance) {
+            this.from = from;
+            this.to = to;
+            this.distance = distance;
+        }
+        
+        public int compareTo(Vertex o) {
+            return this.distance.compareTo(o.distance);
+        }
+    }
+    
     public int minCostConnectPoints(int[][] points) {
-        int n = points.length, ans = 0;
-        HashSet<Integer> mst = new HashSet<>();
-        mst.add(0);
-        int[] dist = new int[n];
-        for(int i = 1; i < n; i++) dist[i] = findDist(points, 0, i);
-        while(mst.size() != n) {
-            // Find the node that has shortest distance
-            int next = -1;
-            for(int i = 0; i < n; i++) {
-                if(mst.contains(i)) continue;
-                if(next == -1 || dist[next] > dist[i]) next = i;
-            }
-            // Put the node into the Minning Spanning Tree
-            mst.add(next);
-            ans += dist[next];
-            // Update distance array
-            for(int i = 0; i < n; i++) {
-                if(!mst.contains(i)) {
-                    dist[i] = Math.min(dist[i], findDist(points, i, next));
-                }
+        
+        int[] union = new int[points.length];
+        for (int i = 0; i < union.length; i++) {
+            union[i] = i;
+        }
+        
+        PriorityQueue<Vertex> pq = new PriorityQueue<>();
+        for (int i = 0; i < points.length; i++) {
+            for (int j = 0; j < points.length; j++) {
+                if (i == j) continue;
+                int[] a = points[i];
+                int[] b = points[j];
+                pq.add(new Vertex(i, j, distance(a, b)));
             }
         }
-        return ans;
+        
+        int minCost = 0;
+        while (!pq.isEmpty()) {
+            Vertex cur = pq.poll();
+            int from = cur.from;
+            int to = cur.to;
+            int distance = cur.distance;
+            
+            if (union[from] == union[to]) continue;
+            int toValue = union[to];
+            for (int i = 0; i < union.length;i++) {
+                if (toValue != union[i]) continue;
+                union[i] = union[from];
+            }
+            minCost += distance;
+        }
+        
+        return minCost;
     }
-    private int findDist(int[][] points, int a, int b) {
-        return Math.abs(points[a][0] - points[b][0]) + Math.abs(points[a][1] - points[b][1]);
+   
+    
+    private int distance(int[] a, int[] b){
+        int x1 = a[0];
+        int y1 = a[1];
+        int x2 = b[0];
+        int y2 = b[1];
+        
+        return Math.abs(x1-x2) + Math.abs(y1-y2);
     }
 }
