be609cef33e4fe0ee21c24dd78c2851307d2a9b9
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-09-24 07:05:04 +0900

    Time: 89 ms (10.54%), Space: 43 MB (85.52%) - LeetHub

--- LeetCode/1048-longest-string-chain/1048-longest-string-chain.java
+++ LeetCode/1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,39 +1,40 @@
 class Solution {
     public int longestStrChain(String[] words) {
-        // Step 1: Sort the words array by length.
-        Arrays.sort(words, (String a, String b) -> a.length() - b.length());
-
-        // Step 2: Create a HashMap to store word-to-length relationships.
-        Map<String, Integer> dp = new HashMap<>();
-
-        // Initialize the maximum path length to 1 (a single word).
-        int maxPath = 1;
-
-        // Step 3: Iterate through the words.
-        for (String word : words) {
-            int currLength = 1; // Initialize the current word length to 1.
-            StringBuilder sb = new StringBuilder(word);
-
-            // Step 4: Iterate through each character of the current word.
-            for (int i = 0; i < word.length(); i++) {
-                sb.deleteCharAt(i); // Remove the current character.
-                String prevWord = sb.toString(); // Get the predecessor word.
-
-                // Update the current word length using dynamic programming.
-                currLength = Math.max(currLength, dp.getOrDefault(prevWord, 0) + 1);
-
-                // Restore the original word for the next iteration.
-                sb.insert(i, word.charAt(i));
-            }
-
-            // Step 5: Store the current word and its calculated length in the map.
-            dp.put(word, currLength);
-
-            // Update the maximum path length if necessary.
-            maxPath = Math.max(maxPath, currLength);
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        if (words.length == 1) return 1;
+        for (String word: words) {
+            System.out.print(word + " ");
         }
 
-        // Step 6: Return the maximum path length.
-        return maxPath;
+        int predecessor = 0;
+        int result = 0;
+        int dp[] = new int[words.length];
+        dp[0] = 1;
+        for (int i = 1; i < words.length; i++) {
+            String wordB = words[i];
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                String wordA = words[j];
+                if(isPredecessor(wordA, wordB)) dp[i] = Math.max(dp[j] + 1, dp[i]) ;
+                result = Math.max(dp[i], result);
+                // System.out.print(dp[i] + " ");
+            }
+            // System.out.println(" ");
+        }
+
+        return result;
+    }
+
+    private boolean isPredecessor(String wordA, String wordB) {
+
+        int refA = 0;
+        int refB = 0;
+        if (wordA.length() != wordB.length() - 1) return false;
+
+        while (refA < wordA.length() && refB < wordB.length()) {
+            if(wordA.charAt(refA) == wordB.charAt(refB)) refA++;
+            refB++;
+        }
+        return refA == wordA.length();
     }
 }
