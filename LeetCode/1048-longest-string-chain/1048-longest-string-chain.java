770a926ca810cafac1daa515806b88d0fc9f9626
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-09-25 07:41:21 +0900

    Time: 41 ms (36.85%), Space: 43.8 MB (30.38%) - LeetHub

--- LeetCode/1048-longest-string-chain/1048-longest-string-chain.java
+++ LeetCode/1048-longest-string-chain/1048-longest-string-chain.java
@@ -5,7 +5,7 @@ class Solution {
 
 
         int predecessor = 0;
-        int result = 0;
+        int result = 1;
         int dp[] = new int[words.length];
         dp[0] = 1;
         for (int i = 1; i < words.length; i++) {
@@ -13,11 +13,10 @@ class Solution {
             dp[i] = 1;
             for (int j = 0; j < i; j++) {
                 String wordA = words[j];
+                if (wordA.length() != wordB.length() - 1) continue;
                 if(isPredecessor(wordA, wordB)) dp[i] = Math.max(dp[j] + 1, dp[i]) ;
                 result = Math.max(dp[i], result);
-                // System.out.print(dp[i] + " ");
             }
-            // System.out.println(" ");
         }
 
         return result;
