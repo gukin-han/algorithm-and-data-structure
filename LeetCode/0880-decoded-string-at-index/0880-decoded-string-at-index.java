8943e8330c0068c08e855cb615ee86ba248eaf1d
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-09-28 07:08:56 +0900

    Time: 1 ms (31.44%), Space: 40.4 MB (56.19%) - LeetHub

--- LeetCode/0880-decoded-string-at-index/0880-decoded-string-at-index.java
+++ LeetCode/0880-decoded-string-at-index/0880-decoded-string-at-index.java
@@ -1,26 +1,27 @@
 class Solution {
     public String decodeAtIndex(String s, int k) {
-        long totalLength = 0;
+        
+        Long totalLength = 0L;
+        
+        // 1. Calculate the total length;
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (Character.isDigit(c)) totalLength *= (int) c - '0';
-            else totalLength++;
+            else totalLength ++;
         }
         
+        // 2. Decode in Reverse
         for (int i = s.length() - 1; i >= 0; i--) {
             char c = s.charAt(i);
             if (Character.isDigit(c)) {
                 totalLength /= (int) c - '0';
                 k %= totalLength;
-            } else {
-                if (k == totalLength || k == 0) return "" + s.charAt(i);
-               totalLength--; 
-            } 
-            
-            
+            } else { // isLetter
+                if (k == totalLength || k == 0) return "" + c;
+                totalLength --;
+            }
         }
         
         return "";
-        
     }
 }
