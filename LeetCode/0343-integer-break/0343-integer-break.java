b10eb242085b6fc2c5a9d9d7aa65688eb55dcfef
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-10-07 07:33:35 +0900

    Time: 0 ms (100.00%), Space: 39.6 MB (11.87%) - LeetHub

--- LeetCode/0343-integer-break/0343-integer-break.java
+++ LeetCode/0343-integer-break/0343-integer-break.java
@@ -1,13 +1,15 @@
 class Solution {
     public int integerBreak(int n) {
-        if (n == 2) return 1;
-        if (n == 3) return 2;
+        if (n <= 3) {
+            return n-1;
+        }
         int[] dp = new int[58+1];
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 3;
         
         for (int i = 4; i <= n; i++) {
+
             dp[i] = Math.max(dp[i - 3] * 3, dp[i-2] * 2);
         }
         
