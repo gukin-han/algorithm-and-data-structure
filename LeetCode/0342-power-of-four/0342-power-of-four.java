cd2e06205987fcb779aa6ee8f1ff7e492fdd0145
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-10-23 09:58:19 +0900

    Time: 25 ms (76.31%), Space: 41.2 MB (6.52%) - LeetHub

---
+++ LeetCode/0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n == 1) return true;
+        int x = 1; 
+        for (int i = 1; i < 16; i++) {
+            x = x << 2;
+            System.out.println(n);
+            if (n == x) return true;
+        }
+         return false;
+        
+    }
+}
