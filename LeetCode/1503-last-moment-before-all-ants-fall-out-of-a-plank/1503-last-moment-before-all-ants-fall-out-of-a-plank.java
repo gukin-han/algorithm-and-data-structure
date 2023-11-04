8d63324747693c4cf7f8b23adc96538823caca40
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-11-04 10:45:14 +0900

    Time: 6 ms (16.48%), Space: 44.1 MB (9.89%) - LeetHub

--- LeetCode/1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.java
+++ LeetCode/1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.java
@@ -2,25 +2,20 @@ import java.util.OptionalInt;
 
 class Solution {
     public int getLastMoment(int n, int[] left, int[] right) {
-        int max = findMaxLeft(left);
-        int min = findMinRight(right);
-
-        if (max == -1) return n - min;
-        if (min == -1) return max;
-        else {
-            return Math.max(max - 0, n - min);
+        int result = -1;
+        
+        if (left.length != 0) {
+            Arrays.sort(left);
+            result = left[left.length - 1];
         }
+        
+        if (right.length != 0) {
+            Arrays.sort(right);
+            result = Math.max(result, n - right[0]);
+        }
+        
+        return result;
+
     }
 
-    int findMaxLeft(int[] left) {
-        OptionalInt max = Arrays.stream(left).max();
-        if (max.isPresent()) return max.getAsInt();
-        else return -1;
-    }
-
-    int findMinRight(int[] right) {
-        OptionalInt min = Arrays.stream(right).min();
-        if (min.isPresent()) return min.getAsInt();
-        else return -1;
-    }
 }
