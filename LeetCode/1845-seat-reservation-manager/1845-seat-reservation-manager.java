65ddaad22c36f3b40d3102f29ac903c0b5e26d91
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-11-06 19:00:10 +0900

    Time: 113 ms (9.84%), Space: 94.7 MB (7.17%) - LeetHub

--- LeetCode/1845-seat-reservation-manager/1845-seat-reservation-manager.java
+++ LeetCode/1845-seat-reservation-manager/1845-seat-reservation-manager.java
@@ -1,12 +1,10 @@
 class SeatManager {
     private TreeSet<Integer> unReservedSeats = new TreeSet<>();
-    private boolean[] isTaken;
     
     public SeatManager(int n) {
         for (int i = 1; i <= n ; i++) {
             unReservedSeats.add(i);
         }
-        isTaken = new boolean[n + 1];
     }
     
     public int reserve() {
