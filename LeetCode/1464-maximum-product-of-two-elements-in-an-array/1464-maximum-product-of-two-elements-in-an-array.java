f0bb239ba79f688d0c221b9b83821086deffae80
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-12-12 10:07:38 +0900

    Time: 0 ms (100.00%), Space: 42 MB (33.98%) - LeetHub

--- LeetCode/1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ LeetCode/1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -1,7 +1,20 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length - 1;
-        return (nums[n] - 1) * (nums[n-1] - 1);
+        // Arrays.sort(nums);
+        // int n = nums.length - 1;
+        // return (nums[n] - 1) * (nums[n-1] - 1);
+        
+        int max = 0;
+        int prevMax = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= max) {
+                prevMax = max;
+                max = nums[i];
+            } else if (nums[i] >= prevMax && nums[i] <= max) {
+                prevMax = nums[i];
+            }
+        }
+        
+        return (max - 1) * (prevMax - 1);
     }
 }
