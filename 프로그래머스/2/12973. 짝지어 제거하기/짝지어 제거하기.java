5df430a669d93f173cba2e36e3516b4ac8004537
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-12-15 22:27:42 +0900

    [level 2] Title: 짝지어 제거하기, Time: 57.66 ms, Memory: 61.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+        for (char c: s.toCharArray()) {
+            if (stack.isEmpty() || stack.peek() != c) stack.push(c);
+            else if (stack.peek() == c) {
+                stack.pop();
+            }
+        }
+        
+
+        return stack.isEmpty() ? 1:0;
+    }
+}
