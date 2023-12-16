047d1f074812795b6e795fe546a9ab6135561085
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-12-16 12:41:49 +0900

    Time: 2 ms (98.39%), Space: 43.3 MB (66.13%) - LeetHub

--- LeetCode/0242-valid-anagram/0242-valid-anagram.java
+++ LeetCode/0242-valid-anagram/0242-valid-anagram.java
@@ -2,17 +2,16 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
         
-        Map<Character, Integer> map = new HashMap<>();
+        int[] count = new int[(int) 'z' + 1];
         
-        for (int i= 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        for (char c: s.toCharArray()) {
+            count[(int) c]++;
         }
         
-        for (int i = 0; i < t.length(); i++) {
-            char ch = t.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) - 1);
-            if (map.get(ch) < 0) return false;
+        for (char c: t.toCharArray()) {
+            int index = (int) c;
+            count[index]--;
+            if (count[index] < 0) return false; 
         }
         
         return true;
