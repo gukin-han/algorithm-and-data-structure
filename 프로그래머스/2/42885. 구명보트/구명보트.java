edf15c9736c76f8271be427e8818b19f826ecbd9
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-12-18 12:15:45 +0900

    [level 2] Title: 구명보트, Time: 9.02 ms, Memory: 53.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int l = 0;
+        int r = people.length - 1;
+        int answer = 0;
+        
+        while (l <= r) {
+            if (people[l] + people[r] > limit) {
+                r--;
+                answer++;
+            } else {
+                l++;
+                r--;
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
