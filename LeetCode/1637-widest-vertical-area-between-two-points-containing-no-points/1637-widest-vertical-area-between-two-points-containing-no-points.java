8f64673c4f6448946e12a1f680bf76af84f44482
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-12-21 15:43:51 +0900

    Time: 39 ms (21.54%), Space: 72.9 MB (5.00%) - LeetHub

--- LeetCode/1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.java
+++ LeetCode/1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.java
@@ -1,14 +1,14 @@
 class Solution {
     public int maxWidthOfVerticalArea(int[][] points) {
-        Arrays.sort(points, (o1, o2) -> {
-            return o1[0] - o2[0];
-        });
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
 
-        int result = 0;
+        int max_width = 0;
+
         for (int i = 1; i < points.length; i++) {
-            result = Math.max(points[i][0]-points[i-1][0], result);
+            int width = points[i][0] - points[i - 1][0];
+            max_width = Math.max(max_width, width);
         }
 
-        return result;
+        return max_width;
     }
 }
