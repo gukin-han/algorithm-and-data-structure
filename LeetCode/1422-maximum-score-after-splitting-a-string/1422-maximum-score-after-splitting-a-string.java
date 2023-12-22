0910ea6c5bd3f82ae1a0021656f5925d05479ed6
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2023-12-22 11:06:18 +0900

    Time: 1 ms (97.78%), Space: 41.2 MB (33.33%) - LeetHub

--- LeetCode/1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
+++ LeetCode/1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -1,22 +1,23 @@
 class Solution {
     public int maxScore(String s) {
-        int zeros = 0;
         int ones = 0;
+        int zeros = 0;
+        int best = Integer.MIN_VALUE;
         
-        for (char c: s.toCharArray()) {
-            if (c == '0') zeros++;
-            else ones++;
-        }
-        
-        int result = 0;
-        int left = 0;
-        int right = ones;
         for (int i = 0; i < s.length() - 1; i++) {
-            if (s.charAt(i) == '0') left++;
-            else right--;
-            result = Math.max(result, left + right);
+            if (s.charAt(i) == '1') {
+                ones++;
+            } else {
+                zeros++;
+            }
+            
+            best = Math.max(best, zeros - ones);
         }
         
-        return result;
+        if (s.charAt(s.length() - 1) == '1') {
+            ones++;
+        }
+        
+        return best + ones;
     }
 }
