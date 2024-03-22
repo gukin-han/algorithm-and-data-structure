594812066625f259363abbb7cbff66c3cfe7967f
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2024-03-22 22:21:26 +0900

    Time: 163 ms (5.15%), Space: 62 MB (66.72%) - LeetHub

--- LeetCode/0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ LeetCode/0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,33 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-
-    public ListNode reverse(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-        while(curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
-
     public boolean isPalindrome(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head.next;
-        while(fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        int size = 0;
+        ListNode cur = head;
+        while (cur != null) {
+            cur = cur.next;
+            size++;
         }
-        ListNode rev = reverse(slow.next); // reverse second list
-        slow.next = null;
-        while(rev != null) {
-            if(head.val != rev.val) {
-                return false;
-            }
+        
+        if (size == 1) return true;
+
+        int mid = (size % 2 == 0 ? size / 2 + 1: size / 2 + 2);
+        ListNode head2 = head;
+        for (int i = 0; i < mid - 1; i++) {
+            head2 = head2.next;
+        }
+        cur = head2.next;
+        ListNode prev = null;
+        while (cur != null) {
+            prev = cur;
+            cur = cur.next;
+            prev.next = head2;
+            head2 = prev;
+        }
+
+        for (int i = 0; i < size/2; i++) {
+            System.out.println(head.val + " " + head2.val);
+            if (head.val != head2.val) return false;
             head = head.next;
-            rev = rev.next;
+            head2 = head2.next;
         }
+
         return true;
     }
 }
