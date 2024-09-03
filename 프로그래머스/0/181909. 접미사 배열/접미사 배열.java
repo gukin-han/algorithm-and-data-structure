07e9d15b7c4d62c5a287856f5f2b666262405a6e
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2024-09-03 22:55:16 +0900

    [level 0] Title: 접미사 배열, Time: 0.51 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        int size = my_string.length();
+        String[] answer = new String[size];
+        for (int i = 0; i < size; i++){
+            answer[i] = my_string.substring(i, size);
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
