70724b702255fb6c05d0c791de4bef223687c7c5
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2024-10-09 18:20:30 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,14 +1,7 @@
 -- 코드를 입력하세요
 SELECT
-pg.price_group
-, count(p.product_id) as products
-from product p
-inner join (
-    select
-    product_id
-    , (price div 10000) * 10000 as price_group
-    from product
-) pg
-on p.product_id = pg.product_id
-group by pg.price_group
-order by pg.price_group asc
+(price div 10000) * 10000 as price_group
+, count(product_id) as products
+from product
+group by price_group
+order by price_group asc
