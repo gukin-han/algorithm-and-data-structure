e35a4cee775e973ccd4f9d6a2f686d46b2504f52
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2024-11-02 17:26:45 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,14 +1,14 @@
-WITH RECURSIVE cte AS (
-  SELECT id, parent_id, 0 AS depth 
-  FROM ecoli_data 
-  WHERE parent_id IS NULL
-
-  UNION ALL
-
-  SELECT e.id, e.parent_id, c.depth + 1 
-  FROM ecoli_data e
-  JOIN cte c ON e.parent_id = c.id
-  WHERE c.depth <= 2
+-- 코드를 작성해주세요
+with recursive cte as(
+    select id, 0 as depth 
+    from ECOLI_DATA
+    where parent_id is null
+    
+    union all
+    
+    select ed.id, cte.depth + 1 from cte
+    inner join ECOLI_DATA ed
+    on cte.id = ed.parent_id where cte.depth < 2
 )
 
-SELECT id FROM cte where depth = 2 order by id asc;
+select id from cte where depth = 2
