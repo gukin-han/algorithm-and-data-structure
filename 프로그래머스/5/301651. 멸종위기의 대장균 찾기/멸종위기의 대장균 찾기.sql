d70cb9b5814489007a5d6f10c34f91b0c54d5533
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2024-11-02 22:16:44 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,15 +1,17 @@
 -- 코드를 작성해주세요
 with recursive cte as (
-    select e.id, e.parent_id, 1 generation from ECOLI_DATA e where e.parent_id is null
+    select id, parent_id, 1 generation  from ECOLI_DATA where parent_id is null
     
     union all
     
-    select e.id, e.parent_id, cte.generation + 1 from ECOLI_DATA e
-    inner join cte on cte.id = e.parent_id
+    select child.id, child.parent_id, parent.generation + 1 from cte parent 
+    inner join ECOLI_DATA child 
+    on parent.id = child.parent_id
 )
 
-select count(*) count, c1.generation from cte c1
-left join cte sub on c1.id = sub.parent_id
-where sub.id is null
-group by c1.generation
-order by c1.generation
+select count(*) count, parent.generation from cte parent
+left join cte child
+on parent.id = child.parent_id
+where child.id is null
+group by parent.generation
+order by parent.generation
