053a44a58f3a99d8abdfcd439652445cf71937dc
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2024-11-02 11:35:16 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,30 +1,16 @@
 -- 코드를 입력하세요
-with most_review_count as(
-    select
-        count(*) max_count
-    from rest_review
-    group by member_id
-    order by count(*) desc
+with most_frequent_reviewer as (
+    select member_id, count(*)
+    from REST_REVIEW
+    group by MEMBER_ID
+    order by count(*) desc 
     limit 1
-), 
-review_count_member_id as(
-    select 
-        rr.member_id,
-        count(*) review_count
-    from REST_REVIEW rr
-    inner join MEMBER_PROFILE mp on rr.member_id = mp.member_id
-    group by rr.member_id
-),
-most_reviewed_member_id as(
-    select
-        rcm.member_id
-    from review_count_member_id rcm
-    cross join most_review_count mrc
-    where rcm.review_count = mrc.max_count
 )
 
-select mp.member_name, rr.review_text, date_format(rr.review_date, '%Y-%m-%d') review_date from REST_REVIEW rr
-inner join member_profile mp on mp.member_id = rr.member_id
-inner join most_reviewed_member_id mrm on mrm.member_id = mp.member_id
-order by rr.review_date, rr.review_text
-
+select member_name, review_text, date_format(review_date, '%Y-%m-%d') as review_date
+from REST_REVIEW rr
+inner join MEMBER_PROFILE mp
+on rr.member_id = mp.member_id
+inner join most_frequent_reviewer mfr
+on rr.member_id = mfr.member_id
+order by review_date asc, review_text asc
